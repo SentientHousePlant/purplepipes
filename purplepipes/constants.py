6bfa960a9021675f7064b5d1cc4a213f72492a3f
Lils Harrington <lilsharrington@gmail.com>
2022-06-16 22:05:24 +0100

    :sparkles: Very basic schema written.

---
+++ purplepipes/constants.py
@@ -0,0 +1,24 @@
+import jsonschema
+
+purple_yaml_schema = {
+    "type" : "object",
+    "properties" : {
+        "name" : {"type":"string"},
+        "input" : {"type":"string"},
+        "steps" : {"type":"object"},
+        "output" : {"type" : "string", "enum" : ["csv", "json"]},
+        "output path" : {"type" : "string"}
+    },
+    "required":["name", "input", "output", "output path"]
+}
+
+if __name__ == "__main__":
+
+    test_dict = {
+        "name" : "test",
+        "input" : "hi",
+        "output" : "csv",
+        "output path": "somewhere"
+    }
+
+    jsonschema.validate(schema=purple_yaml_schema, instance=test_dict)
