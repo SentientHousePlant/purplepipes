a8b42be5d3c44281856a4190c62580e7b28dcac1
Indi Harrington <indigoharrington@gmail.com>
2022-07-08 21:29:41 +0100

    :bug: Fixed validation function error.
    Validation errors were not being caught because of double handling.

--- purplepipes/main.py
+++ purplepipes/main.py
@@ -1,7 +1,7 @@
 import click
-import jsonschema
 from purplepipes.utils import parse_yaml
 from purplepipes.constants import DEFAULT_PAML_NAME
+from jsonschema.exceptions import ValidationError
 
 
 @click.group()
@@ -19,5 +19,5 @@ def validate():
     try:
         parse_yaml()
         print(f"{DEFAULT_PAML_NAME} is a valid purplepipes config file.")
-    except jsonschema.ValidationError() as e:
-        print(f"Invalid Schema : {e}")
+    except ValidationError as e:
+        print(f"Invalid Schema : {e.message}")
--- purplepipes/utils.py
+++ purplepipes/utils.py
@@ -10,8 +10,5 @@ paml_validate = partial(jsonschema.validate, schema=test_paml_schema)
 def parse_yaml(file_name=DEFAULT_PAML_NAME):
     with open(file_name) as paml:
         loaded_yaml = safe_load(paml)
-    try:
         paml_validate(instance=loaded_yaml)
         return loaded_yaml
-    except jsonschema.ValidationError as e:
-        return e
